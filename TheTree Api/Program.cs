1dd0c4aeef88011acd8e6a766482a380e3669013
JachimczykTomasz <LordUssop@gmail.com>
2019-11-04 17:40:20 +0100

    TheTree Initial commit

---
+++ TheTree Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheTree_Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TheTree Core/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheTree_Core.Models
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateOfCreation { get; set; }
+        public DateTime DateOfChange { get; set; }
+    }
+}
