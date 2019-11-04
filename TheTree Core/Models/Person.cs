9ee40818a9b193e9a97bde2d817cd526eac87d5b
Jacek Jachimczyk <jacek.jachimczyk@gmail.com>
2019-11-04 21:49:33 +0100

    add first entities

---
+++ TheTree Core/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheTree_Core.Models
+{
+	public class Person : BaseEntity
+	{
+		//[Required]
+		public string Name;
+		//[Required]
+		public string Surname;
+		public Relationship ParenthoodId;
+	}
+}
---
+++ TheTree Core/Models/Relationship.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheTree_Core.Models
+{
+	public class Relationship : BaseEntity
+	{
+		//[Required]
+		public Person HusbandId;
+		//[Required]
+		public Person WifeId;
+		public bool IsActual; //current state of marriage -> false - divorced; true - ?actual?
+	}
+}
