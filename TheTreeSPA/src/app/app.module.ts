43140bac6419ce14377935d700ebe8ec7a5e9629
JachimczykTomasz <LordUssop@gmail.com>
2019-11-19 16:58:02 +0100

    Add SPA

---
+++ TheTreeSPA/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './_components/login-screen/login-screen.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginScreenComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
