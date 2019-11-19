a67a3f665115b8c8dddad6dba73b9799add41bdf
Jacek Jachimczyk <jacek.jachimczyk@gmail.com>
2019-11-19 19:43:15 +0100

    SPA Service - ListAll method

--- TheTreeSPA/src/app/_components/person/person.component.ts
+++ TheTreeSPA/src/app/_components/person/person.component.ts
@@ -18,7 +18,7 @@ export class PersonComponent implements OnInit {
 
   ngOnInit() 
   {
-    this.service.ListAll().subscribe(response => persons = response)
+    this.service.ListAll().subscribe(response => this.persons = response)
   }
 
 }
--- TheTreeSPA/src/app/_services/person.service.ts
+++ TheTreeSPA/src/app/_services/person.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Person } from '../_models/person';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,9 @@ import { Injectable } from '@angular/core';
 export class PersonService {
 
   constructor() { }
+  
+  public ListAll(): Observable<Person[]>
+  {
+    return;
+  }
 }
